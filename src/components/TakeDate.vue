15482d0c91750888539dc5786905e7d810fd2cd4
runzac <91915526+alerunza@users.noreply.github.com>
2023-11-01 12:06:18 +0100

    fix form error

--- src/components/TakeDate.vue
+++ src/components/TakeDate.vue
@@ -1,32 +1,13 @@
-<!-- <template>
-    <v-container>
-        <v-row justify="center">
-            <v-col cols="12" sm="6" md="4">
-                <v-form ref="form" @submit.prevent>
-                    <v-text-field v-model="firstName" label="Nome" required></v-text-field>
-                    <v-text-field v-model="lastName" label="Cognome" required></v-text-field>
-                    <v-text-field v-model="email" label="Email" required type="email"></v-text-field>
-                    <VueDatePicker class="pa-4" v-model="date" locale="it" disable-month-year-select
-                        :enable-time-picker="false" cancelText="Annulla" selectText="Seleziona"
-                        placeholder="Seleziona una data e un orario">
-                    </VueDatePicker>
-                    <v-btn color="success" type="submit">Prenota</v-btn>
-                </v-form>
-            </v-col>
-        </v-row>
-    </v-container>
-</template> -->
-
 <template>
-    <v-sheet width="500" class="mx-auto pa-16">
-        <v-form fast-fail @submit.prevent>
+    <v-sheet width="400" class="mx-auto pa-16">
+        <v-form fast-fail ref="form">
             <v-text-field v-model="firstName" label="Nome" :rules="firstNameRules"></v-text-field>
 
             <v-text-field v-model="lastName" label="Cognome" :rules="lastNameRules"></v-text-field>
             <VueDatePicker class="pa-4" v-model="date" locale="it" disable-month-year-select :enable-time-picker="false"
                 cancelText="Annulla" selectText="Seleziona" placeholder="Seleziona una data e un orario">
             </VueDatePicker>
-            <v-btn type="submit" color="success" block class="mt-2">Prenota</v-btn>
+            <v-btn type="submit" color="success" block class="mt-2" @click="submitForm" disabled>Prenota</v-btn>
         </v-form>
     </v-sheet>
 </template>
@@ -72,7 +53,7 @@ export default {
     },
     methods: {
         submitForm() {
-            if (this.$refs.form.validate() && this.isValidDate(this.date)) {
+            if (this.$refs.form.validate() && this.date) {
                 alert('Prenotazione effettuata con successo');
             } else {
                 alert('Per favore, compila tutti i campi obbligatori e seleziona una data.');
