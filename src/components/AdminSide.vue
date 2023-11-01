0c25ad4bc266483f4eb7ec84dde9267011fdc9a0
runzac <91915526+alerunza@users.noreply.github.com>
2023-11-01 15:42:46 +0100

    add adminside + axios to get from serverside (API)

---
+++ src/components/AdminSide.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <h1>Lista delle Prenotazioni</h1>
+        <ul>
+            <li v-for="prenotazione in prenotazioni" :key="prenotazione.id">
+                {{ prenotazione.nome }} {{ prenotazione.cognome }} - {{ prenotazione.data }}
+            </li>
+        </ul>
+    </div>
+</template>
+  
+<script>
+import axios from 'axios';
+
+export default {
+    data() {
+        return {
+            prenotazioni: [],
+        };
+    },
+    created() {
+        axios.get('http://localhost:3000/api/prenotazioni')
+            .then(response => {
+                this.prenotazioni = response.data;
+                console.log(this.prenotazioni);
+            })
+            .catch(error => {
+                console.error('Errore nel recupero delle prenotazioni:', error);
+            });
+    },
+};
+</script>
